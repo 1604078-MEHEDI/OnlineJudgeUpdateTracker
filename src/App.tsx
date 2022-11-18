9c44b6a61ec649edc026d72694d6e4d3d9877559
1604078-MEHEDI <hasanmahadi877@gmail.com>
2022-11-18 09:54:19 +0600

    Add signUp and SignIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import Login from "./components/Login";
+import Sign from "./components/SignIn";
 
 function App() {
   return (
     <div className="App">
       <div>Online Judge Update Tracker</div>
-      <Login />
+      {/* <SignUp /> */}
+      <Sign />
     </div>
   );
 }
